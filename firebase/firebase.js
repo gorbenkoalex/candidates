d6cbdf5268045516c0790daa32ccc78cb41f0eb6
Oleksii Horbenko <oleksiihorbenko@MacBook-Pro-Oleksii.local>
2024-07-10 22:38:16 +0300

    candidates

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,23 +2,23 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref as refDb, set, child, push, get, update } from "firebase/database";
 import { getStorage, ref as refStorage, uploadBytes, getDownloadURL } from "firebase/storage";
-import {string} from "yup";
-// import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDKpdNkHdAN1d1BTCXgflh97Eqo-wJcabc",
-    authDomain: "candidates-e1234.firebaseapp.com",
-    projectId: "candidates-e1234",
-    storageBucket: "candidates-e1234.appspot.com",
-    messagingSenderId: "882210310006",
-    appId: "1:882210310006:web:80b17356862687f3e11c57",
-    measurementId: "G-P9XZM1SES9",
-    databaseURL: "https://candidates-e1234-default-rtdb.firebaseio.com/",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+    databaseURL: "",
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
